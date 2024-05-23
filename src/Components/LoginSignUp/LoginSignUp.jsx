aeb73296d4a6d9c70a33f18c3e78332f2259bd5b
Fellype Oliveira <113564265+1fellype@users.noreply.github.com>
2024-05-23 01:17:50 -0300

    main

---
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './LoginSignUp.css'
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+ const LoginSignUp = () => {
+  return (
+    <div className='container'>
+        <div className="header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+        </div>
+
+        <div className="inputs">
+            <div className="input">
+                <img src={user_icon} alt="Imagem de Usuário" />
+                <input type="text" />
+            </div>
+            <div className="input">
+                <img src={email_icon} alt="Email do Usuário" />
+                <input type="email" />
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="Senha do Usuário" />
+                <input type="password" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginSignUp
